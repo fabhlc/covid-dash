fe48716c310b0f3abecd8a9af501f0fd3550e488
Fabienne <fabienne.chan@properly.ca>
2020-04-07 19:29:47 -0400

    Remove debug
    is it solving heroku timeout?

--- covid_dash.py
+++ covid_dash.py
@@ -345,5 +345,5 @@ def update_deathsdf(prov, region):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True,
-                   dev_tools_hot_reload_interval=40_000)
+    app.run_server()
+    
